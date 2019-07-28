c4da8d89bab5e27e20efd303ba22026afa088ba4
ManuelMedina88 <anglane10@gmail.com>
2019-07-28 08:04:01 -0400

    I created a controller and a welcome_page

--- practice_4_crud/src/main/java/com/newtech/applicant/app/controllers/IndexController.java
+++ practice_4_crud/src/main/java/com/newtech/applicant/app/controllers/IndexController.java
@@ -1,10 +1,20 @@
 package com.newtech.applicant.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/home")
 public class IndexController {
 	
-	
-
+	@GetMapping("/index")
+	public ModelAndView showIndex(ModelAndView model) {
+		
+		model.addObject("title", "My Site");
+		model.addObject("greeting", "Hello from the backend");
+		model.setViewName("welcome_page");
+		return model;
+	}
 }
