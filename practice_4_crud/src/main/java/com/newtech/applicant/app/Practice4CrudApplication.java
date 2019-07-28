e485bc69cb5e6ff9e6ebaabdcd63e5fa6e119a0e
ManuelMedina88 <anglane10@gmail.com>
2019-07-28 07:51:10 -0400

    I just created the project

---
+++ practice_4_crud/src/main/java/com/newtech/applicant/app/Practice4CrudApplication.java
@@ -0,0 +1,13 @@
+package com.newtech.applicant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice4CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice4CrudApplication.class, args);
+	}
+
+}
---
+++ practice_4_crud/src/main/java/com/newtech/applicant/app/controllers/IndexController.java
@@ -0,0 +1,10 @@
+package com.newtech.applicant.app.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+	
+	
+
+}
